4705bf5aeaadd1006ff89b28f9441d909ce7415f
Esteban <ejsandoval@uc.cl>
2023-11-03 18:05:09 -0300

    Adding content and pages.

---
+++ src/content/config.ts
@@ -0,0 +1,31 @@
+// Import utilities from `astro:content`
+import { z, defineCollection } from "astro:content";
+// Define a `type` and `schema` for each collection
+const postsCollection = defineCollection({
+  type: "content",
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      year: z.string(),
+      cover: image().refine((img) => img.width >= 1080, {
+        message: "Cover image must be at least 1080 pixels wide!",
+      }),
+      coverAlt: z.string(),
+      tags: z.array(z.string()),
+      team: z.array(z.string()).optional(),
+      color: z.string(),
+      images: z
+        .array(
+          image().refine((img) => img.width >= 480, {
+            message: "Image must be at least 4080 pixels wide!",
+          })
+        )
+        .optional(),
+      alts: z.array(z.string()).optional(),
+    }),
+});
+
+// Export a single `collections` object to register your collection(s)
+export const collections = {
+  projects: postsCollection,
+};
